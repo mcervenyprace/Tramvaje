f568ae15907af393d4f5a6dc73eaf72c3e68cef9
mcervenyprace <mcervenyprace@seznam.cz>
2015-09-14 16:17:11 +0200

    Data pro simulaci

---
+++ src/simulace/run.java
@@ -0,0 +1,30 @@
+package simulace;
+
+public class run {
+
+	public static void main(String args[]){
+		
+		
+		// pole s nazvy zastavek 
+		
+		final String[] poleZastavek1 = {"Ejpovicka","Uzlova","Makova","Kvetova"};
+		final String[] poleZastavek2 = {"Vokata","Uzlova","Mrnava"};
+		
+		// pole s vzdalenostmi zastavek (v minutach)
+		
+		final int[] poleVzdalenosti1 = {4,5,3};
+		final int[] poleVzdalenosti2 = {6,7};
+		
+		// pole s variancemi zpozdeni
+
+		final int[] poleZpozdeni1 = {2,6,7};
+		final int[] poleZpozdeni2 = {2,13};
+			
+		
+		
+		
+			System.out.println("Vitejte v Martinovo simulaci tramvaji!");
+		}
+			
+	
+}
